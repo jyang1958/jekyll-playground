886b6175e923a8f1d0b85368836e461c4918c8f1
Kyle Han <kyhan316@gmail.com>
2016-03-15 06:28:42 -0400

    Commented on bootstrap_examples.js

--- public/LivingProgram/2016/03/15/bootstrap_examples.js
+++ public/LivingProgram/2016/03/15/bootstrap_examples.js
@@ -1,20 +1,20 @@
-function bootstrap_examples(container, data){
-  container.selectAll('div')
-    .data(data)
-    .enter()
-    .append('div')
-    .each(function (d) {
-      var s = d3.select(this);
-      s.append('h2')
-       .append('a')
+function bootstrap_examples(container, data){ //add two links to every example, one that leads to bootstraps website the other to a  github person's code
+  container.selectAll('div') //selecting all the div created from the bootstrap-examples.html, so basically all the examples
+    .data(data) 
+    .enter()  //enter the data
+    .append('div')  
+    .each(function (d) { //for each of the examples
+      var s = d3.select(this); //this referes to the example
+      s.append('h2') //append a header
+       .append('a')  //with a embedded link
        .attr('href', function (d) { 
-         return 'https://getbootstrap.com/examples/' + d; 
+         return 'https://getbootstrap.com/examples/' + d; //this link leads to the bootstrap page examples
        })
-      .html(function (d) { return d;});
-      s.append('a')
+      .html(function (d) { return d;}); //the header has the name of the example as text
+      s.append('a') //append another link
        .attr('href', function (d) { 
-         return 'https://github.com/twbs/bootstrap/tree/gh-pages/examples/' + d; 
+         return 'https://github.com/twbs/bootstrap/tree/gh-pages/examples/' + d; //this time it leads to a github guy's page
        })
-      .html('GitHub source');
+      .html('GitHub source'); //name all of the links github source
     });
 }
