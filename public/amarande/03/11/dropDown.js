1a5d7f493ea3309cc1a1aa8cc5286a1681cf669a
amarande <amarande@users.noreply.github.com>
2016-03-16 11:19:46 -0400

    Create dropDown.js

---
+++ public/amarande/03/11/dropDown.js
@@ -0,0 +1,33 @@
+---
+div:
+  attr:
+    class: dropdown
+button:
+  attr:
+    class: btn btn-default dropdown-toggle
+    type: button
+    id: dropdownMenu
+    data-toggle: dropdown
+    aria-haspopup: true
+    aria-expanded: true
+span:
+  attr:
+    class: caret
+ul:
+  attr: 
+    class: dropdown-menu
+    aria-labelledby: dropdownMenu
+---
+function dropdown(container, data) {
+  var div = container.append('div').attr({{ page.div.attr | jsonify}});
+  var button = div.append('button').attr({{ page.button.attr | jsonify }}).html('Dropdown');
+  var span = button.append('span').attr({{ page.span.attr | jsonify }});
+  var ul = div.append('ul').attr({{ page.ul.attr | jsonify }});
+  ul.selectAll('li')
+    .data(data)
+    .enter()
+    .append('li')
+    .append('a')
+    .attr('href', function (d) { return d.url; })
+    .html(function (d) { return d.text;} ); 
+}
