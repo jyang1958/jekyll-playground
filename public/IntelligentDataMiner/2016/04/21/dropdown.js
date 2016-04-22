cb4904890fbbdffd2424e39cfb5a026738d48cd3
IntelligentDataMiner <ethan.c.kwok@gmail.com>
2016-04-21 21:36:21 -0400

    Update dropdown.js

--- public/IntelligentDataMiner/2016/04/21/dropdown.js
+++ public/IntelligentDataMiner/2016/04/21/dropdown.js
@@ -18,16 +18,16 @@ ul:
     class: dropdown-menu
     aria-labelledby: dropdownMenu
 ---
-function dropdown(container, data){
-  var div= container.append('div').attr({{ page.div.attr | jsonify}});
-  var button= div.append('button').attr({{ page.button.attr | jsonify}}).html("Dropdown");
-  var span = button.append('span').attr({{ page.span.attr |jsonify}});
-  var ul = div.append('ul').attr({{ page.ul.attr | jsonify}});
+function dropdown(container, data) {
+  var div = container.append('div').attr({{ page.div.attr | jsonify}});
+  var button = div.append('button').attr({{ page.button.attr | jsonify }}).html("Dropdown");
+  var span = button.append('span').attr({{ page.span.attr | jsonify }});
+  var ul = div.append('ul').attr({{ page.ul.attr | jsonify }});
   ul.selectAll('li')
     .data(data)
     .enter()
     .append('li')
     .append('a')
-    .attr('href', function (d) { return d.url;})
-    .html(function (d) { return d,text;});
+    .attr('href', function (d) { return d.url; })
+    .html(function (d) { return d,text;} );
 }
